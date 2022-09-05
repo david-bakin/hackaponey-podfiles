9bb7af5d26641a12ff2b3cb7f64d6c58e4d53319
David Bakin <david@bakins-bits.com>
2022-09-05 13:52:23 -0700

    oops, typo in last commit

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ echo _source_dir: ${_source_dir}
 
 for _file in '.default.gitpod.yml' '.default.gitpod.Dockerfile'; do {
     _file_final="${_file#.default}";
-    echo _file: ${file}, _file_final: ${file_final};
+    echo _file: ${_file}, _file_final: ${_file_final};
     if test -e "$_source_dir/$_file_final" && test ! -e "$GITPOD_REPO_ROOT/$_file_final"; then {
         echo cp: copying $_source_dir/$_file to $GITPOD_REPO_ROOT/$_file_final;
         cp "$_source_dir/$_file" "$GITPOD_REPO_ROOT/$_file_final";

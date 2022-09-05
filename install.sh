30f1e4699c468e481648bf4d611e1b58c6ee3400
David Bakin <david@bakins-bits.com>
2022-09-05 13:57:25 -0700

    Fix variable reference (bug)

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ echo _source_dir: ${_source_dir}
 for _file in '.default.gitpod.yml' '.default.gitpod.Dockerfile'; do {
     _file_final="${_file#.default}";
     echo _file: ${_file}, _file_final: ${_file_final};
-    if test -e "$_source_dir/$_file_final" && test ! -e "$GITPOD_REPO_ROOT/$_file_final"; then {
+    if test -e "$_source_dir/$_file" && test ! -e "$GITPOD_REPO_ROOT/$_file_final"; then {
         echo cp: copying $_source_dir/$_file to $GITPOD_REPO_ROOT/$_file_final;
         cp "$_source_dir/$_file" "$GITPOD_REPO_ROOT/$_file_final";
     } fi
